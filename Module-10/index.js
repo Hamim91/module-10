7df04fc3e3d28700ebe8a2ec964c5e4020cc93d2
Thohidul Islam Hamim <144516839+Hamim91@users.noreply.github.com>
2023-10-09 20:34:14 +0600

    Module-10

---
+++ Module-10/index.js
@@ -0,0 +1,55 @@
+var a = 20;
+var b = 30;
+var c = a+b;
+
+console.log("addition c = ", c);
+
+
+var a = 30;
+var b = 20;
+var c = a-b;
+
+console.log("substraction c = ", c);
+
+var a = 20;
+var b = 10;
+var c = a*b;
+
+console.log("multiplication  c = ", c);
+
+var a = 50;
+var b = 2;
+var c = a/b;
+
+console.log("divison  c = ", c);
+
+var name = "My Name is Thohidul Islam Hamim"
+
+console.log(typeof name);
+console.log(name.toUpperCase());
+
+//.3
+
+var marks = 63;
+
+if(marks >= 80){
+console.log("A+")
+
+}else if(marks >=75){
+    console.log("A")
+
+}else if( marks>=70){
+    console.log("A-")
+
+}else if( marks>=60-65){
+    console.log("B+")
+
+}else if( marks>=50){
+    console.log("B")
+
+}else if( marks>=40){
+    console.log("C");
+}
+else{ 
+    console.log("fail.")
+}
